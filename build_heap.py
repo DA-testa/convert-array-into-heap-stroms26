0ec3f162bc217e5b5deac21a343c8708a5a7c271
Haralds Strombergs <121034489+stroms26@users.noreply.github.com>
2023-03-19 21:19:15 +0000

    test1

--- build_heap.py
+++ build_heap.py
@@ -3,37 +3,52 @@
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+        n = len(arr)
+
+    for i in range(n // 2, -1, -1):
+        min_heapify(i, arr, swaps)
 
 
     return swaps
 
 
+    def min_heapify(i, arr, swaps):
+    n = len(arr)
+
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    smallest = i
+
+    if left < n and arr[left] < arr[smallest]:
+        smallest = left
+    if right < n and arr[right] < arr[smallest]:
+        smallest = right
+    if smallest != i:
+        arr[i], arr[smallest] = arr[smallest], arr[i]
+        swaps.append((i, smallest))
+        min_heapify(smallest, arr, swaps)
+
+
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    input_type = input()
+    if "I" in input_type:
+        n = int(input())
+        arr = list(map(int, input().split()))
+    elif "F" in input_type:
+        file_name = input()
+        if "a" not in file_name:
+            with open('./tests/'+file_name, 'r') as f:
+                n = int(f.readline())
+                arr = list(map(int, f.readline().split()))
 
+    assert len(arr) == n
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    swaps = build_heap(arr)
 
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
+    assert len(swaps) <= 4 * n
     print(len(swaps))
+
     for i, j in swaps:
         print(i, j)
 
